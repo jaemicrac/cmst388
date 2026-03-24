7835b124a4942ea8ef3b084cf1215c9299164f68
Jaemi <135385091+jaemicrac@users.noreply.github.com>
2026-03-23 20:25:31 -0400

    Update project1.js

--- js/project1.js
+++ js/project1.js
@@ -41,13 +41,14 @@ document.addEventListener('DOMContentLoaded', () => {
     --------------------------------------------------
     */
 
-    document.querySelector("#add-classes").addEventListener('click', () => {
+   document.querySelector("#add-classes").addEventListener('click', () => {
         document.querySelector('.selector-examples li:first-child').classList.add('first');
         document.querySelectorAll('.selector-examples li:nth-child(even)').forEach(el => el.classList.add('even'));
         document.querySelectorAll('.selector-examples li:nth-child(odd)').forEach(el => el.classList.add('odd'));
         document.querySelector('.selector-examples li:nth-child(4)').classList.add('highlighter');
         document.querySelector('.selector-examples li:nth-child(5)').classList.add('highlighter');
         document.querySelector('.selector-examples li:last-child').classList.add('last');
+    });
  // TODO: Add remaining selectors using nth-child, loops and conditional logic where approriate. (https://www.w3schools.com/CSSref/sel_nth-child.php) 
        
     });
